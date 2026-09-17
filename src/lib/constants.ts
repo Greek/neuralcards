33f42ce448b44d2e0a2e76f657462e68c382f6dd
Andreas <git@fsck.rip>
2026-09-17 18:23:33 -0400

    feat: add auth
    Signed-off-by: Andreas <git@fsck.rip>

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const APP_NAME: string = "NeuralCards";
+export const MIN_PASSWORD_LENGTH = /.{8,}/;
+export const MAX_PASSWORD_LENGTH = 128;
+export const SPECIAL_CHARACTERS = /[!@#$%^&*(),.?":{}|<>]/;
--- src/server/better-auth/client.ts
+++ src/server/better-auth/client.ts
@@ -1,5 +1,17 @@
-import { createAuthClient } from "better-auth/react";
+import { createAuthClient, type ErrorContext } from "better-auth/react";
 
 export const authClient = createAuthClient();
 
 export type Session = typeof authClient.$Infer.Session;
+
+export const translateAuthErrorCode = (error: ErrorContext) => {
+  switch (error.error.code) {
+    case 'USER_ALREADY_EXISTS':
+      return 'A user with that email address already exists.';
+    case 'INVALID_EMAIL_OR_PASSWORD':
+      return error.error.message;
+    default:
+      return error.error.message;
+  }
+};
+
